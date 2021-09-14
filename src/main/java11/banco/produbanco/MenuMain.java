2fec8c02b58669e37d776d545ec1f72a447390b4
Diana Guashca <dianaguashca18@gmail.com>
2021-09-13 20:22:03 -0500

    puebaaa cuatrooo

--- src/main/java11/banco/produbanco/MenuMain.java
+++ src/main/java11/banco/produbanco/MenuMain.java
@@ -5,45 +5,48 @@ import java.util.Scanner;
 public class MenuMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner lector= new Scanner(System.in);
-		int opcion=0;
-		int clave=85;
+		// TODO Auto-generated method st
 		
-			System.out.println("Ingrese clave ");
-			opcion=lector.nextInt();
-			do {
+		Scanner lector=new Scanner(System.in);
+		String clave="";
+		String clave1="85AB";
+		String Salir="-1";
+		do {
+			System.out.println("Ingrese la clave");
+			clave=lector.nextLine();
+			if(clave.equals(clave1)) {
 				
-				if(opcion!=clave) {
-					System.out.println("la clave no es correcta");
-					opcion=lector.nextInt();
+				
+				int opcion=0;
+				do {
 					
-				}
-				if (opcion==clave) {
-				System.out.println("Banco Produbanco ");
-				System.out.println("1.-Realizar transferencia");
-				System.out.println("2.-consultar transacciones ");
-				System.out.println("3.-Salir ");
-				System.out.println("elija opcion ");
+				
+				System.out.println("BANCO PRODUBANCO, elija una opción:");
+				System.out.println("1. Realizar transferencia");
+				System.out.println("2. Consultar transacciones");
+				System.out.println("3. SALIR");
 				opcion=lector.nextInt();
-				}
-				
 				if(opcion==1) {
-					int saldo=1000;
-					System.out.println("su nuevo saldo es "+saldo);
-					opcion=lector.nextInt();
-				}
-					if(opcion==2) {
-						System.out.println("“En este momento se están procesando las"
-								+ " transacciones, intente más tard");
-						opcion=lector.nextInt();
-					}
 				
-		
-			}while(opcion!=3);
+				System.out.println("Ingrese una cuenta:");
+				lector.nextLine();
+				String cuenta=lector.nextLine();
+				System.out.println("Ingrese monto:");
+			int monto=lector.nextInt();
+				int  SaldoD=1000;
+				int N=SaldoD-monto;
+				System.out.println("Su nuevo saldo es:"+N);
+					
+				}else if(opcion==2) {
+					System.out.println("“En este momento se están procesando las transacciones,"
+							+ " intente más tarde");
+				}
+				}while(opcion!=3);
+				
+			}else {
+				System.out.println("la clave incorrecta ");
+			}
 			
-
-			System.out.println("Vuelva pronto");
-	}
-
-}
+		}while(!clave.equals(Salir));
+		System.out.println("“Gracias por preferirnos”.");
+	}}
