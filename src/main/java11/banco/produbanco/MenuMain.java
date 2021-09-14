2041cf44c959a09916f64f4ff21175bb150fdf1c
Diana Guashca <dianaguashca18@gmail.com>
2021-09-13 20:26:13 -0500

    puebaaa produbanco

--- src/main/java11/banco/produbanco/MenuMain.java
+++ src/main/java11/banco/produbanco/MenuMain.java
@@ -13,40 +13,55 @@ public class MenuMain {
 		String Salir="-1";
 		do {
 			System.out.println("Ingrese la clave");
+			
 			clave=lector.nextLine();
 			if(clave.equals(clave1)) {
-				
-				
 				int opcion=0;
 				do {
 					
 				
-				System.out.println("BANCO PRODUBANCO, elija una opción:");
-				System.out.println("1. Realizar transferencia");
-				System.out.println("2. Consultar transacciones");
-				System.out.println("3. SALIR");
+				    System.out.println("BANCO PRODUBANCO");
+				    System.out.println(" elija una opción:");
+				    
+				    System.out.println("1. Realizar transferencia");
+				    System.out.println("2. Consultar transacciones");
+				    System.out.println("3. SALIR");
+				    
 				opcion=lector.nextInt();
+				
 				if(opcion==1) {
 				
 				System.out.println("Ingrese una cuenta:");
 				lector.nextLine();
 				String cuenta=lector.nextLine();
+				
 				System.out.println("Ingrese monto:");
-			int monto=lector.nextInt();
-				int  SaldoD=1000;
-				int N=SaldoD-monto;
+				
+				
+			    int monto=lector.nextInt();
+				int  Saldo=1000;
+				int N=Saldo-monto;
+				
 				System.out.println("Su nuevo saldo es:"+N);
 					
 				}else if(opcion==2) {
+					
 					System.out.println("“En este momento se están procesando las transacciones,"
 							+ " intente más tarde");
 				}
+				
 				}while(opcion!=3);
 				
 			}else {
+				
 				System.out.println("la clave incorrecta ");
 			}
 			
+			
 		}while(!clave.equals(Salir));
+		
+		
 		System.out.println("“Gracias por preferirnos”.");
-	}}
+	}
+	
+}
